f8bc32361651c75d0c1846ad9c3912414e047408
Koura <koura@Mahtikone.(none)>
2014-02-24 23:47:33 +0200

    some done

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,6 +2,10 @@ class Brewery < ActiveRecord::Base
   include RatingAverage
   validates :name, presence: true
   validates :year, numericality: { less_than_or_equal_to: ->(_) { Time.now.year} , only_integer: true}
+  
+  scope :active, -> {where active:true}
+  scope :retired, -> {where active:[nil, false]}
+  
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 end
